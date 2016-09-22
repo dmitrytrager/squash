c0eac2af9833dd11f582b24256aa69fd36b85907
Dmitry Trager <dmitry.trager@flatstack.com>
2016-09-22 17:26:06 +0300

    Update Rails to newest 3 version and Gemfile.lock

--- Gemfile.d/framework.rb
+++ Gemfile.d/framework.rb
@@ -1,4 +1,4 @@
-gem "rails", github: "rails/rails", branch: "3-2-stable"
+gem "rails", "3.2.9"
 # We need to use this branch of Rails because it includes fixes for ActiveRecord
 # and concurrency that we need for our thread-spawning background job paradigm
 # to work
